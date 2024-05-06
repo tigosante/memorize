c50240fb1f6ed19c58dd1f75502c1cc86384ade9
Tiago Silva <tsilvasantos38@gmail.com>
2024-05-05 21:09:18 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tiago Silva on 05/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
